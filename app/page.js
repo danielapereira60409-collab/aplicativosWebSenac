a722873f8e89e2fc5d02dbb0e62e4b3136e244a7
danielapereira60409-collab <danielapereira60409@gmail.com>
2026-04-22 14:25:15 -0300

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -19,27 +19,31 @@ export default function () {
       {/* Links */}
       <div className="mt-6 w-full max-w-md flex flex-col gap-3">
 
-        <Link href="/Produto" className="bg-blue-500 text-white text-center py-2 rounded-lg hover:bg-blue-600 transition">
+        <Link href="/Produto" className="bg-gray-500 text-white text-center py-2 rounded-lg hover:bg-blue-600 transition">
           Cadastro de Produtos
         </Link>
 
-        <Link href="/Cliente" className="bg-green-500 text-white text-center py-2 rounded-lg hover:bg-green-600 transition">
+        <Link href="/Cliente" className="bg-gray-500 text-white text-center py-2 rounded-lg hover:bg-green-600 transition">
           Cadastro de Clientes
         </Link>
 
-        <Link href="/Fornecedores" className="bg-purple-500 text-white text-center py-2 rounded-lg hover:bg-purple-600 transition">
+        <Link href="/tarefas" className="bg-gray-500 text-white text-center py-2 rounded-lg hover:bg-gray-600 transition">
+          Gerenciador de Tarefas
+        </Link>
+
+        <Link href="/Fornecedores" className="bg-gray-500 text-white text-center py-2 rounded-lg hover:bg-gray-600 transition">
           Cadastro de Fornecedores
         </Link>
 
-        <Link href="/IMC" className="bg-yellow-500 text-white text-center py-2 rounded-lg hover:bg-yellow-600 transition">
+        <Link href="/IMC" className="bg-gray-500 text-white text-center py-2 rounded-lg hover:bg-yellow-600 transition">
           Calcular IMC
         </Link>
 
-        <Link href="/idade" className="bg-pink-500 text-white text-center py-2 rounded-lg hover:bg-pink-600 transition">
+        <Link href="/idade" className="bg-gray-500 text-white text-center py-2 rounded-lg hover:bg-pink-600 transition">
           Calcular Idade
         </Link>
 
-        <Link href="/ficha" className="bg-red-600 text-white text-center py-2 rounded-lg hover:bg-red-700 transition">
+        <Link href="/ficha" className="bg-gray-500 text-white text-center py-2 rounded-lg hover:bg-red-700 transition">
           Ficha de Cadastro
         </Link>
       <Link href="/crud" className="bg-gray-500 text-white text-center py-2 rounded-lg hover:bg-gray-600 transition">
