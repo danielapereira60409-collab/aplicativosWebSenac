1abc1eefae9234c2a883fde87e6a9ee876c3e202
danielapereira60409-collab <danielapereira60409@gmail.com>
2026-04-22 15:03:29 -0300

    Add files via upload

--- app/tarefas/page.js
+++ app/tarefas/page.js
@@ -1,5 +1,4 @@
 "use client";
-import Link from "next/link";
 import { useState } from "react";
 
 export default function Home() {
@@ -8,6 +7,7 @@ export default function Home() {
   const [hora, setHora] = useState("");
   const [status, setStatus] = useState("Pendente");
   const [lista, setLista] = useState([]);
+  const [proximoId, setProximoId] = useState(1);
 
   function adicionarTarefa(e) {
     e.preventDefault();
@@ -18,16 +18,16 @@ export default function Home() {
     }
 
     const nova = {
-      id: Date.now(),
-      tarefa,
-      data,
-      hora,
-      status,
+      id: proximoId,
+      tarefa: tarefa,
+      data: data,
+      hora: hora,
+      status: status,
     };
 
-    setLista([...lista, nova]);
+    setLista(lista.concat(nova));
+    setProximoId(proximoId + 1);
 
-    // limpar
     setTarefa("");
     setData("");
     setHora("");
@@ -35,110 +35,102 @@ export default function Home() {
   }
 
   function remover(id) {
-    const novaLista = lista.filter((item) => item.id !== id);
+    const novaLista = lista.filter(function(item) {
+      return item.id !== id;
+    });
     setLista(novaLista);
   }
 
   return (
-    <main className="min-h-screen bg-gray-500 p-6 flex justify-center">
-      <div className="w-full max-w-4xl bg-black p-6 rounded-xl shadow">
+    <main className="min-h-screen bg-gray-200 p-6 flex justify-center">
+      <div className="w-full max-w-4xl bg-white p-6 rounded-xl shadow">
 
-        {/* Título */}
-        <h1 className="text-2xl font-bold mb-2">Minhas Tarefas</h1>
-        <p className="text-gray-500 mb-6">
-          Gerencie suas tarefas de forma simples
-        </p>
+        <h1 className="text-2xl font-bold mb-4 text-center text-black">
+          Minhas Tarefas
+        </h1>
 
-        {/* Formulário */}
-        <form onSubmit={adicionarTarefa} className="space-y-4">
+        <form onSubmit={adicionarTarefa} className="space-y-4 placeholder:text-gray-500 ">
 
-          <div>
-            <label className="block font-medium">Tarefa</label>
+          <input
+            type="text"
+            placeholder="Digite a tarefa"
+            value={tarefa}
+            onChange={(e) => setTarefa(e.target.value)}
+            className="w-full border p-2 text-black rounded placeholder:text-black"
+          />
+
+          <div className="grid grid-cols-2 gap-4 text-black">
             <input
-              type="text"
-              placeholder="Descreva a tarefa..."
-              value={tarefa}
-              onChange={(e) => setTarefa(e.target.value)}
-              className="w-full border p-2 rounded"
+              type="date"
+              value={data}
+              onChange={(e) => setData(e.target.value)}
+              className="border p-2 rounded placeholder:text-gray-500"
+            />
+
+            <input
+              type="time"
+              value={hora}
+              onChange={(e) => setHora(e.target.value)}
+              className="border p-2 rounded placeholder:text-gray-500"
             />
           </div>
 
-          <div className="grid grid-cols-2 gap-4">
-            <div>
-              <label>Data</label>
-              <input
-                type="date"
-                value={data}
-                onChange={(e) => setData(e.target.value)}
-                className="w-full border p-2 rounded"
-              />
-            </div>
+          <select
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+            className="w-full border p-2 rounded text-black placeholder:text-gray-500"
+          >
+            <option>Pendente</option>
+            <option>Em andamento</option>
+            <option>Concluído</option>
+          </select>
 
-            <div>
-              <label>Horário</label>
-              <input
-                type="time"
-                value={hora}
-                onChange={(e) => setHora(e.target.value)}
-                className="w-full border p-2 rounded"
-              />
-            </div>
-          </div>
-
-          <div>
-            <label>Status</label>
-            <select
-              value={status}
-              onChange={(e) => setStatus(e.target.value)}
-              className="w-full border p-2 rounded"
-            >
-              <option>Pendente</option>
-              <option>Em andamento</option>
-              <option>Concluído</option>
-            </select>
-          </div>
-
-          <button className="bg-blue-500 text-black px-4 py-2 rounded">
-            + Adicionar
+          <button className="bg-blue-500 text-black px-4 py-2 rounded w-full">
+            Adicionar Tarefa
           </button>
         </form>
 
-        {/* Lista */}
-        <div className="mt-8">
-          <h2 className="font-semibold mb-2">Tarefas cadastradas</h2>
+        <div className="mt-6">
+          <h2 className="font-semibold mb-2 text-black">Lista</h2>
 
-          <table className="w-full text-left border">
-            <thead className="bg-gray-500">
-              <tr>
-                <th className="p-2">Tarefa</th>
-                <th className="p-2">Data</th>
-                <th className="p-2">Hora</th>
-                <th className="p-2">Status</th>
-                <th className="p-2">Ações</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {lista.map((item) => (
-                <tr key={item.id} className="border-t">
-                  <td className="p-2">{item.tarefa}</td>
-                  <td className="p-2">{item.data}</td>
-                  <td className="p-2">{item.hora}</td>
-                  <td className="p-2">{item.status}</td>
-                  <td className="p-2">
-                    <button
-                      onClick={() => remover(item.id)}
-                      className="text-red-500"
-                    >
-                      Excluir
-                    </button>
-                  </td>
+          {lista.length === 0 ? (
+            <p className="text-gray-500">Nenhuma tarefa cadastrada</p>
+          ) : (
+            <table className="w-full border text-center">
+              <thead className="bg-blue-500 text-black">
+                <tr>
+                  <th>Tarefa</th>
+                  <th>Data</th>
+                  <th>Hora</th>
+                  <th>Status</th>
+                  <th>Ação</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
 
+              <tbody>
+                {lista.map(function(item) {
+                  return (
+                    <tr key={item.id} className=" text-black border-t">
+                      <td>{item.tarefa}</td>
+                      <td>{item.data}</td>
+                      <td>{item.hora}</td>
+                      <td>{item.status}</td>
+                      <td>
+                        <button
+                          onClick={() => remover(item.id)}
+                          className="text-red-500"
+                        >
+                          Excluir
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
         </div>
+
       </div>
     </main>
   );
